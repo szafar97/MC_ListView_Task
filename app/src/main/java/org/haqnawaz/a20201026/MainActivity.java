d066f87cd3a4a7cfd0b9cc87cc58f0fa6eca67b1
Zubia Abbas <zubiaabbas14@gmail.com>
2020-10-28 10:32:37 +0500

    Given Task Completed

--- app/src/main/java/org/haqnawaz/a20201026/MainActivity.java
+++ app/src/main/java/org/haqnawaz/a20201026/MainActivity.java
@@ -7,11 +7,15 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> friendArrayList;
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         ListView listView = findViewById(R.id.myListView);
 
-        final ArrayList<String> friendArrayList = new ArrayList<String>();
+        friendArrayList = new ArrayList<String>();
 
         friendArrayList.add("Abdullah");
         friendArrayList.add("Azhar");
@@ -30,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         friendArrayList.add("Salman");
         friendArrayList.add("Zahid");
 
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+        arrayAdapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,friendArrayList );
 
         listView.setAdapter(arrayAdapter);
@@ -45,5 +49,14 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+    }
+
+    public void addName(View view) {
+        EditText nameElement = findViewById(R.id.personName);
+        String name = nameElement.getText().toString();
+
+        if (!name.equals(""))
+            arrayAdapter.add(name);
+        nameElement.setText("");
     }
 }
